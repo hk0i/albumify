6f5266cb1b369b1756b0b55b69e6da7a479b9680
Gregory <gmcquillan@gmail.com>
2022-01-19 00:00:35 -0800

    Initial Commit

---
+++ Albumify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Albumify
+//
+//  Created by Gregory McQuillan on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
